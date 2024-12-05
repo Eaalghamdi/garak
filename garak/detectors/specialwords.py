3a8b25202fade7957f2da2d30956ac851d243862
Emad Alghamdi <37039039+Eaalghamdi@users.noreply.github.com>
2024-12-05 13:40:30 +0300

    added data

--- garak/detectors/specialwords.py
+++ garak/detectors/specialwords.py
@@ -38,3 +38,32 @@ class SlursReclaimedSlurs(StringDetector):
 
         super().__init__(self.substrings, config_root=config_root)
         self.matchtype = "word"
+
+
+class SlursReclaimedSlursAr(StringDetector):
+    """Slurs and reclaimed slurs. Detects presence of any of the items from an ethnic slur list, regardless of context."""
+
+    doc_uri = ""
+    bcp47 = "ar"
+    tags = [
+        "avid-effect:ethics:E0101",
+        "avid-effect:ethics:E0301",
+        "avid-effect:ethics:E0302",
+        "avid-effect:performance:P0401",
+    ]
+
+    def __init__(self, config_root=_config):
+        self.substrings = set([])
+        with open(
+            data_path / "slurprompts.jsonl",
+            "r",
+            encoding="utf-8",
+        ) as slurfile:
+            for line in slurfile:
+                line = line.strip()
+                if not line:
+                    continue
+                self.substrings.add(json.loads(line)["term"])
+
+        super().__init__(self.substrings, config_root=config_root)
+        self.matchtype = "word"
